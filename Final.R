824f27e0e11cbe973e5d859436f1f6cc990f1faa
Scott <scottaustinyoung@gmail.com>
2025-11-05 11:59:08 -0500

    Added section for models.

--- Final.R
+++ Final.R
@@ -39,9 +39,9 @@ clean_df <- clean_df %>%
     size_kb = case_when(
       str_detect(size, "M") ~ as.numeric(str_remove(size, "M")) * 1024,
       str_detect(size, "k") ~ as.numeric(str_remove(size, "k")),
-                 TRUE ~ NA_real_)
+                 TRUE ~ NA_real_
+      )
     )
-  )
 
 # Imputing the missing Size values that were introduced in the previous step.
 median_size <- median(clean_df$size_kb, na.rm = TRUE)
@@ -62,3 +62,65 @@ clean_df <- clean_df %>%
 
 # Printing a head table of the cleaned data.
 knitr::kable(head(clean_df))
+
+# Selection of the variables used in our models.
+model_df <- clean_df %>%
+  select(
+    rating,
+    reviews,
+    size_kb,
+    installs,
+    type,
+    price,
+    content_rating,
+    category
+  ) %>%
+  na.omit()
+
+# Creating the training index, train set, and test set.
+set.seed(2)
+train_index <- createDataPartition(model_df$rating, p = 0.6, list = FALSE)
+train_set <- model_df[train_index, ]
+test_set <- model_df[-train_index, ]
+
+# Ten-fold cross-validation setup.
+train_control <- trainControl(method = "cv", number = 10)
+
+# Fitted Model Section.
+# Model 1 Linear Regression between ratings and reviews.
+set.seed(2)
+model_1 <- train(
+  rating ~ reviews + installs,
+  data = train_set,
+  method = "lm",
+  trControl = train_control,
+  metric = "RMSE"
+)
+print(model_1)
+print(model_1$results)
+
+# Model 2 Linear Regression across all variables.
+set.seed(2)
+model_2 <- train(
+  rating ~ .,
+  data = train_set,
+  method = "lm",
+  trControl = train_control,
+  metric = "RMSE"
+)
+print(model_2)
+print(model_2$results)
+
+# Model 3 Random Forest
+set.seed(2)
+model_3 <- train(
+  rating ~ .,
+  data = train_set,
+  method = "rf",
+  trControl = train_control,
+  metric = "RMSE",
+  tuneGrid = expand.grid(.mtry = c(2, 4, 7)),
+  importance = TRUE
+)
+print(model_3)
+print(model_3$results)
